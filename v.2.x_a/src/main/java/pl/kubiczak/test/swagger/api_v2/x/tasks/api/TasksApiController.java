4db190d536aa2f9efa5b0c07c4835e799d5b6254
Wiiitek <kubiczak.jakub@gmail.com>
2018-11-18 15:09:54 +0100

    implement task endpoints

--- v.2.x_a/src/main/java/pl/kubiczak/test/swagger/api_v2/x/tasks/api/TasksApiController.java
+++ v.2.x_a/src/main/java/pl/kubiczak/test/swagger/api_v2/x/tasks/api/TasksApiController.java
@@ -2,10 +2,18 @@ package pl.kubiczak.test.swagger.api_v2.x.tasks.api;
 
 import com.google.common.base.Preconditions;
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import pl.kubiczak.test.swagger.api_v2.x.tasks.entities.TaskEntity;
 import pl.kubiczak.test.swagger.api_v2.x.tasks.repository.TaskRepository;
@@ -14,6 +22,7 @@ import pl.kubiczak.test.swagger.v2.x.a.generated.tasks.model.TaskInput;
 import pl.kubiczak.test.swagger.v2.x.a.generated.tasks.model.TaskOutput;
 
 @Controller
+@Transactional
 public class TasksApiController implements TasksApi {
 
   private final TaskRepository taskRepository;
@@ -24,6 +33,7 @@ public class TasksApiController implements TasksApi {
       = Preconditions.checkNotNull(taskRepository, "Tasks repository is missing");
   }
 
+  @Override
   public ResponseEntity<TaskOutput> createTask(@Valid @RequestBody TaskInput body) {
     TaskEntity toCreate = new TaskEntity();
     toCreate.setDescription(body.getDescription());
@@ -36,5 +46,55 @@ public class TasksApiController implements TasksApi {
       .createdAt(saved.getCreatedAt());
 
     return ResponseEntity.ok(output);
+  }
+
+  @Override
+  public ResponseEntity<List<TaskOutput>> readTasks() {
+    Spliterator<TaskEntity> spliterator = taskRepository.findAll().spliterator();
+    List<TaskOutput> outputList = StreamSupport.stream(spliterator, false)
+      .map(entity -> new TaskOutput()
+        .description(entity.getDescription())
+        .id(entity.getId())
+        .createdAt(entity.getCreatedAt())
+      )
+      .collect(Collectors.toList());
+    return ResponseEntity.ok(outputList);
+  }
+
+  @Override
+  public ResponseEntity<TaskOutput> updateTask(
+    @PathVariable("uuid") UUID uuid,
+    @Valid @RequestBody TaskInput body) {
+    Optional<TaskEntity> optional = taskRepository.findById(uuid);
+    return optional
+      .map((toUpdate) -> {
+        toUpdate.setDescription(body.getDescription());
+        return toUpdate;
+      })
+      .map(taskRepository::save)
+      .map(saved -> new TaskOutput()
+        .description(saved.getDescription())
+        .id(saved.getId())
+        .createdAt(saved.getCreatedAt())
+      )
+      .map(savedTaskOutput -> ResponseEntity.ok(savedTaskOutput))
+      .orElse(ResponseEntity.notFound().build());
+  }
+
+  @Override
+  public ResponseEntity<TaskOutput> deleteTask(@PathVariable("uuid") UUID uuid) {
+    Optional<TaskEntity> optional = taskRepository.findById(uuid);
+    return optional
+      .map((toDelete) -> {
+        taskRepository.deleteById(uuid);
+        return toDelete;
+      })
+      .map(deletedEntity -> new TaskOutput()
+        .description(deletedEntity.getDescription())
+        .id(deletedEntity.getId())
+        .createdAt(deletedEntity.getCreatedAt())
+      )
+      .map(ResponseEntity::ok)
+      .orElse(ResponseEntity.notFound().build());
   }
 }
